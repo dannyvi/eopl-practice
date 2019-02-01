8251fdcc20ce99f873f0683e073d2a00c16f8a3c
dannyvi <dannyvis@icloud.com>
2019-02-01 18:20:05 +0800

    exercise 5.14

--- chapter5/letrec-lang/interp.scm
+++ chapter5/letrec-lang/interp.scm
@@ -14,14 +14,21 @@
   (require "store.scm")
   (require "data-structures.scm")
   (require "environments.scm")
+  (require (only-in racket void))
 
   (provide value-of-program value-of/k)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
   (define instrument-all (make-parameter #f))
+  (define instrument-stat (make-parameter #t))
 
-
+  (define current-cont 1)
+  (define max-cont 1)
+  (define (stat func)
+    (set! current-cont (func current-cont 1))
+    (if (> current-cont max-cont) (set! max-cont current-cont) (void))
+    )
 ;;;;;;;;;;;;;;;; the interpreter ;;;;;;;;;;;;;;;;
 
   ;; value-of-program : Program -> FinalAnswer
@@ -29,6 +36,8 @@
   (define value-of-program 
     (lambda (pgm)
       (initialize-store!)
+      (set! current-cont 1)
+      (set! max-cont 1)
       (cases program pgm
         (a-program (exp1)
           (value-of/k exp1 (init-env) (end-cont))))))  
@@ -38,12 +47,17 @@
   (define value-of/k
     (lambda (exp env cont)
       (when (instrument-all)
-       (eopl:printf "~n~n(value-of/k ~n ~a ~n ~a ~n ~a)" exp env cont))
+            (eopl:printf "~n~n(value-of/k ~n ~a ~n ~a ~n ~a)" exp env cont))
+      
       (cases expression exp
-        (const-exp (num) (apply-cont cont (num-val num) env))
-        (var-exp (var) (apply-cont cont
-                                   (deref (apply-env env var)) env))
+        (const-exp (num)
+          (stat -)
+          (apply-cont cont (num-val num) env))
+        (var-exp (var)
+          (stat -)
+          (apply-cont cont (deref (apply-env env var)) env))
         (proc-exp (vars body)
+          (stat -)
           (apply-cont cont 
             (proc-val (procedure vars body env)) env))
         (letrec-exp (p-name b-vars p-body letrec-body)
@@ -51,43 +65,63 @@
             (extend-env-rec p-name b-vars p-body env)
             cont))
         (zero?-exp (exp1)
+          (stat +)
           (value-of/k exp1 env
             (zero1-cont cont)))
         (let-exp (var exp1 body)
+          (stat +)
           (value-of/k exp1 env
                       (let-exp-cont var body cont)))
         (letm-exp (vars exprs body)
+          (stat +)
           (let ((exps (reverse exprs)))
             (value-of/k (car exps) env
               (letm-cont vars '() (cdr exps) body cont))))
         (let2-exp (var1 exp1 var2 exp2 body)
+          (stat +)
           (value-of/k exp1 env
             (let2-1-cont var1 var2 exp2 body cont)))
         (if-exp (exp1 exp2 exp3)
+          (stat +)
           (value-of/k exp1 env
             (if-test-cont exp2 exp3 cont)))
         (diff-exp (exp1 exp2)
+          (stat +)
           (value-of/k exp1 env
                       (diff1-cont exp2 cont)))
         (multiply-exp (exp1 exp2)
+          (stat +)
           (value-of/k exp1 env (multiply1-cont exp2 cont)))
-        (call-exp (rator rands) 
+        (call-exp (rator rands)
+          (stat +)
           (value-of/k rator env
                       (rator-cont (reverse rands) cont)))
 
-        (emptylist-exp () (apply-cont cont
-                                      (list-val (empty-leest)) env))
-        (cons-exp (exp1 exp2) (value-of/k exp1 env (cons-cont exp2 cont)))
-        (car-exp (exp1) (value-of/k exp1 env (car-cont cont)))
-        (cdr-exp (exp1) (value-of/k exp1 env (cdr-cont cont)))
-        (null?-exp (exp1) (value-of/k exp1 env  (null?-cont cont)))
+        (emptylist-exp ()
+          (stat -)
+          (apply-cont cont (list-val (empty-leest)) env))
+
+        (cons-exp (exp1 exp2)
+          (stat +)
+          (value-of/k exp1 env (cons-cont exp2 cont)))
+        (car-exp (exp1)
+          (stat +)
+          (value-of/k exp1 env (car-cont cont)))
+        (cdr-exp (exp1)
+          (stat +)
+          (value-of/k exp1 env (cdr-cont cont)))
+        (null?-exp (exp1)
+          (stat +)
+          (value-of/k exp1 env  (null?-cont cont)))
         (list-exp (exprs)
           (let ((exps (reverse exprs)))
             (if (null? exps)
-              (apply-cont cont (list-val (empty-leest)) env)
-              (value-of/k (car exps) env
-                          (list-cont (empty-leest) (cdr exps) cont)))))
+             (begin (stat -) (apply-cont cont (list-val (empty-leest)) env))
+             (begin (stat +)
+               (value-of/k (car exps) env
+                 (list-cont (empty-leest) (cdr exps) cont))))))
         (begin-exp (exp1 exps)
+          (stat +)
           (value-of/k exp1 env (begin-cont exps cont)))
    )))
 
@@ -95,16 +129,22 @@
   ;; Page: 148
   (define apply-cont
     (lambda (cont val saved-env)
+      
       (when (instrument-all)
-       (eopl:printf "~n~n(apply-cont ~n ~a ~n ~a)" cont val))
+            (eopl:printf "~n~n(apply-cont ~n ~a ~n ~a)" cont val))
       (cases continuation cont 
         (end-cont () 
           (begin
+            (stat -)
             (eopl:printf
-              "End of computation.~%")
+             "End of computation.~%")
+            (when (instrument-stat)
+                  (eopl:printf "max count ~a ~n" max-cont)
+                  )
             val))
         ;; or (logged-print val)  ; if you use drscheme-init-cps.scm
         (zero1-cont (saved-cont)
+          (stat -)
           (apply-cont saved-cont
             (bool-val
               (zero? (expval->num val))) saved-env))
@@ -118,12 +158,14 @@
               (value-of/k body
                           (extend-env* vars (cons (newref val) vals) saved-env)
                           saved-cont)
+              (begin (stat +)
               (value-of/k (car exps) saved-env
                 (letm-cont vars (cons (newref val) vals)
-                           (cdr exps) body  saved-cont))
+                           (cdr exps) body  saved-cont)))
               ))
 
         (let2-1-cont (var1 var2 exp2 body saved-cont)
+          (stat +)
           (value-of/k exp2 saved-env
            (let2-2-cont var1 (newref val) var2 body saved-cont)))
 
@@ -137,22 +179,27 @@
              (value-of/k exp2 saved-env saved-cont)
              (value-of/k exp3 saved-env saved-cont)))
         (diff1-cont (exp2 saved-cont)
+          (stat +)
           (value-of/k exp2
             saved-env (diff2-cont val saved-cont)))
         (diff2-cont (val1 saved-cont)
+          (stat -)
           (let ((num1 (expval->num val1))
                 (num2 (expval->num val)))
             (apply-cont saved-cont
                         (num-val (- num1 num2)) saved-env)))
         (multiply1-cont (exp2 saved-cont)
+           (stat +)
            (value-of/k exp2
              saved-env (multiply2-cont val saved-cont)))
         (multiply2-cont (val1 saved-cont)
+           (stat -)
            (let ((num1 (expval->num val1))
                           (num2 (expval->num val)))
                       (apply-cont saved-cont
                                   (num-val (* num1 num2)) saved-env)))
         (rator-cont (rands saved-cont)
+          (stat +)
           (value-of/k (car rands) saved-env
             (rand-cont val (cdr rands) '() saved-cont)))
         (rand-cont (rator-val rands rand-vals saved-cont)
@@ -160,37 +207,46 @@
               (apply-procedure/k (expval->proc rator-val)
                                  (cons val rand-vals)
                                  saved-cont)
-              (value-of/k (car rands) saved-env
+              (begin (stat +)
+                (value-of/k (car rands) saved-env
                 (rand-cont rator-val
                            (cdr rands) (cons val rand-vals)
-                            saved-cont)))
+                            saved-cont))))
                    )
           ;(let ((proc (expval->proc val1)))
           ;  (apply-procedure/k proc val saved-cont)))
         (cons-cont (exp2  saved-cont)
+          (stat +)
           (value-of/k exp2 saved-env (cons2-cont val saved-cont)))
         (cons2-cont (val1 saved-cont)
+          (stat -)
           (apply-cont saved-cont
             (list-val (cons-leest val1 (expval->leest val)))
             saved-env))
         (car-cont (saved-cont)
+          (stat -)
             (apply-cont saved-cont (car-leest (expval->leest val))
                         saved-env))
         (cdr-cont (saved-cont)
+          (stat -)
             (apply-cont saved-cont (cdr-leest (expval->leest val))
                               saved-env))
         (null?-cont (saved-cont)
+          (stat -)
           (apply-cont saved-cont
                       (bool-val (null?-leest (expval->leest val)))
                       saved-env))
         (list-cont (vals exps saved-cont)
           (if (null? exps)
-              (apply-cont saved-cont
-                          (list-val (cons-leest val vals)) saved-env)
+              (begin (stat -)
+               (apply-cont saved-cont
+                          (list-val (cons-leest val vals)) saved-env))
+              (begin (stat +)
               (value-of/k (car exps) saved-env
                 (list-cont (cons-leest val vals)
-                           (cdr exps) saved-cont))))
+                           (cdr exps) saved-cont)))))
         (begin-cont (exps cont)
+          (stat +)
           (if (null? exps) val
               (value-of/k (car exps) saved-env
                           (begin-cont (cdr exps) cont))))
